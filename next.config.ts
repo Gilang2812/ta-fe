0209f05f41972845704179a23d0a729b9ad77077
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2026-01-05 13:19:14 +0700

    chore: update Next.js to version 15.5.2 and adjust related dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig: import("next").NextConfig = {
+const nextConfig = {
+  experimental: {
+    turbo: false,
+  },
   images: {
     remotePatterns: [
-      { protocol: "http", hostname: "localhost" },
-      { protocol: "https", hostname: "img.icons8.com" },
-      { protocol: "https", hostname: "openweathermap.org" },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+      },
       {
         protocol: "http",
         hostname: "openweathermap.org",
