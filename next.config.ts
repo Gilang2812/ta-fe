c928bf972bb65c30a354ea60ae1479e716d1971d
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2026-01-05 12:15:44 +0700

    refactor(next.config.ts): streamline image remote patterns and remove experimental settings

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
+const nextConfig: import("next").NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "img.icons8.com",
-      },
-      {
-        protocol: "https",
-        hostname: "openweathermap.org",
-      },
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "img.icons8.com" },
+      { protocol: "https", hostname: "openweathermap.org" },
       {
         protocol: "http",
         hostname: "openweathermap.org",
